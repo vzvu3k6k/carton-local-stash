1ee9f6e2b9efad1e69f3b08074b94f0f0f97ceeb
vzvu3k6k <vzvu3k6k@gmail.com>
2015-03-09 14:36:28 +0900

    Add test.sh

---
+++ t/test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e # die if any command fails
+set -x # verbose
+
+TEMP_REPO=$(mktemp -d)
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cp "$GIT_ROOT/post-checkout" "$TEMP_REPO"
+cd "$TEMP_REPO"
+git init
+mv post-checkout .git/hooks
+
+git commit --allow-empty -m empty
+
+git checkout -b A
+echo "requires 'A';" > cpanfile
+git add cpanfile && git commit -m A
+mkdir local && touch local/A
+
+git checkout -b B
+[ -f local/A ] || exit 1;
+echo "requires 'B';" > cpanfile
+git add cpanfile && git commit -m B
+touch local/B
+
+git checkout master
+[ ! -d local ] || exit 1;
+
+git checkout A
+[ -f local/A ] || exit 1;
+
+git checkout B
+[ -f local/B ] || exit 1;
+
+rm -rf "$TEMP_REPO"
